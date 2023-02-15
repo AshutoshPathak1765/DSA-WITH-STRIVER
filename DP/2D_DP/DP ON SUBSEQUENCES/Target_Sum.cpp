81e09015625ef4439976a9f63366bcb9929276c2
Ashutosh Kumar Pathak <ashutoshpathak1765@gmail.com>
2023-02-15 20:25:11 +0530

    Commented Code for Target Sum and Added Problem Link and YT Link

---
+++ DP/2D_DP/DP ON SUBSEQUENCES/Target_Sum.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+// Problem Link:- https://www.codingninjas.com/codestudio/problems/target-sum_4127362?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=1
+// YT Link:- https://www.youtube.com/watch?v=zoilQD1kYSg&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=20
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    // dp(i,s)-> Number of ways to make a sum of s using the array elements from [0...i]
+    // final answer-> dp(n,(sum+d)/2)
+    // Recurrence-> dp(i,s)=dp(i-1,s)+dp(i-1,s-arr[i])
+    // Base Case-> dp(i,0)=1 for all i from [0...n-1]
+    // dp(0,arr[0])=1 if arr[0]!=0 and dp(0,0)=2 if arr[0]=2
+    // if sum+d is odd return false
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    //  if sum+d is odd return 0
+    if ((sum + d) & 1)
+        return 0;
+
+    int dp[n][(sum + d) / 2 + 1];
+    const int mod = 1e9 + 7;
+
+    for (int i = 0; i < n; i++)
+        for (int s = 0; s <= (sum + d) / 2; s++)
+            dp[i][s] = 0;
+    // dp(i,0)=1 for all i from [0...n-1]
+    for (int i = 0; i < n; i++)
+        dp[i][0] = 1;
+    // dp[0][arr[0]]=1 if arr[0]<=sum
+    if (arr[0] <= sum)
+        dp[0][arr[0]] = 1;
+    // important edge case if arr[0]=0 then there are two valid subsets {},{0} such that whose sum is zero
+    if (arr[0] == 0)
+        dp[0][0] = 2;
+    // compute the answer for the remaining array
+    for (int i = 1; i < n; i++) {
+        for (int s = 0; s <= (sum + d) / 2; s++) {
+            int op1 = dp[i - 1][s];
+            int op2 = (s >= arr[i]) ? dp[i - 1][s - arr[i]] : 0;
+            dp[i][s] = (op1 + op2);
+        }
+    }
+    // return the final answer
+    return dp[n - 1][(sum + d) / 2];
+}
+
+int targetSum(int n, int target, vector<int>& arr) {
+    // Write your code here.
+    // s1-s2=target
+    // s1+s2=S
+    // s1=(target+S)/2
+    // dp(i,s)-> Number of ways to make a target of s if given elements from [0..i]
+    // final answer-> dp(n-1,(S+target)/2) where S= sum of the array
+    // Recurrence-> dp(i,s)= dp(i-1,s)+dp(i-1,s-arr[0])
+    // Base Case-> dp(i,0)=1
+
+    return countPartitions(n, target, arr);
+
+}
